33f0bf4fee5a8087a52301dfef77bc426f472e03
Andrew Wang <siweiw9@gmail.com>
2026-01-05 03:01:29 -0800

    clean up remaining temp code. log state and play an example

--- ranking.py
+++ ranking.py
@@ -3,6 +3,8 @@ Rank solutions with log frequency and guesses with entropy.
 
 Copyright 2026. Andrew Wang.
 """
+import logging
+from math import log2
 from typing import Tuple
 from functools import partial
 from collections import Counter
@@ -11,9 +13,11 @@ import pandas as pd
 from scipy.stats import entropy
 from wordfreq import zipf_frequency
 
+logger = logging.getLogger(__name__)
+
 
 def _unroll_counts(row: np.ndarray):
-    """Helper function to convert a row into unique counts."""
+    """Convert a row into unique counts with zero padding."""
     unique_counts = np.array(list(Counter(row).values()), dtype=int)
     # Pad all to same length so we can apply along axis.
     amount_short = len(row) - len(unique_counts)
@@ -34,22 +38,32 @@ class Ranking:
         self.reachable = np.ones_like(words, dtype=bool)
         self.log_freq = np.vectorize(partial(
             zipf_frequency, lang='en'), otypes=[float])
+        self._log_state()
+
+    def _log_state(self):
+        """Compute the internal entropy of the reachable space."""
+        total_reachable = np.sum(self.reachable)
+        logger.info('Possibilities: %d', total_reachable)
+        space_bits = 0 if total_reachable == 0 else log2(total_reachable)
+        logger.info('Entropy: %.2f', space_bits)
 
     def update(self, guess: str, squares: str):
-        """Update internal state with guess and resultant squares."""
+        """Update internal state with guess and squares result."""
+        logger.info('Guessed %s with result %s', guess, squares)
         idx = self.index.get_loc(guess)
         squares_non_match = self.patterns[idx, :] != squares
         self.reachable[squares_non_match] = False
+        self._log_state()
 
-    def solutions(self) -> Tuple[np.ndarray, np.ndarray]:
+    def likely_solutions(self) -> Tuple[np.ndarray, np.ndarray]:
         """Rank the most likely solutions based on word frequency."""
         possible = self.words[self.reachable]
         frequencies = self.log_freq(possible)
         sorted_idx = np.argsort(frequencies)[::-1]
         return possible[sorted_idx], frequencies[sorted_idx]
 
-    def guesses(self) -> Tuple[np.ndarray, np.ndarray]:
-        """Rank the probabilistic quality of guesses by entropy."""
+    def informative_guesses(self) -> Tuple[np.ndarray, np.ndarray]:
+        """Rank the probabilistic quality of guesses by entropy (in bits)."""
         # All guesses (axis 0) are included. Exclude unreachable candidates.
         working_set = self.patterns[:, self.reachable]
         counts = np.apply_along_axis(_unroll_counts, 1, working_set)
