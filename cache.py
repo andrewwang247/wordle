d03a1a81ea780cb1ac639b99749ab526e282142e
Andrew Wang <siweiw9@gmail.com>
2026-01-05 13:19:30 -0800

    reorganize caching hierarchy

--- cache.py
+++ cache.py
@@ -1,59 +1,35 @@
 """
 Loading and caching data for words and patterns.
+
 Precompute square responses for all pairings.
 
 Copyright 2026. Andrew Wang.
 """
 import logging
+from os import listdir, path
+from shutil import copyfileobj
 import numpy as np
 from tqdm import tqdm
-from constants import Square
+from constants import wordle_compare
 
 logger = logging.getLogger(__name__)
 
-_DICTIONARY_FILE = 'words.txt'
-_PATTERN_CACHE_FILE = 'patterns.npz'
+_CHUNK_DIR = 'bin/'
+_DICTIONARY_FILE = 'resources/words.txt'
+_PATTERN_ARCHIVE_FILE = 'resources/patterns.npz'
+_PATTERN_CACHE_FILE = 'resources/patterns.npy'
 
 
 def load_words() -> np.ndarray:
     """Load the dictionary array (n,) from words list."""
     words = np.loadtxt(_DICTIONARY_FILE, dtype=str)
     logger.info('Loaded %d words from %s', len(words), _DICTIONARY_FILE)
+    len_vec = np.vectorize(len, otypes=[int])
+    unique_lens = np.unique(len_vec(words))
+    assert unique_lens.shape == (1,), \
+        'Words in dictionary must have uniform length'
+    logger.info('Validated all words have length %d', unique_lens[0])
     return words
-
-
-@np.vectorize
-def _wordle_compare(guess: str, answer: str, pbar: tqdm) -> str:
-    """
-    Given a guess and an answer, generate the squares pattern.
-
-    This runs in a very tight vectorized loop during compilation.
-    """
-    guess_np = np.array(list(guess))
-    answer_np = np.array(list(answer))
-    squares = np.full_like(guess_np, Square.BLACK.value, dtype='<1U')
-
-    # Green is the easiest case to handle by position.
-    # The mask gm is used to remove them in further processing.
-    not_green = guess_np != answer_np
-    squares[~not_green] = Square.GREEN.value
-
-    # We need to get the number (n) of times a guess letter in
-    # a non-green spot occurs in expected. From there, we color
-    # the first (up to) n occurences of the letter yellow in guess.
-    for cand in np.unique(guess_np[not_green]):
-        # For every distinct character in guess not in a green spot,
-        # get the number of times it appears in a non green answer spot.
-        cand_count = np.count_nonzero(answer_np[not_green] == cand)
-        # Mask for where this character appears in guess.
-        matching_spots = guess_np == cand
-        # Get indicies where both not green and guess matches character.
-        possible_idx = np.where(not_green & matching_spots)[0]
-        # Make all of those indices up to cand_count yellow.
-        squares[possible_idx[:cand_count]] = Square.YELLOW.value
-
-    pbar.update(1)
-    return ''.join(squares)
 
 
 def compile_patterns():
@@ -66,15 +42,40 @@ def compile_patterns():
     num_words = len(words)
     logger.info('Cross compiling %d patterns for %d words',
                 num_words**2, num_words)
+    cmp_pat = np.vectorize(wordle_compare, otypes=[str])
     with tqdm(total=num_words**2) as pbar:
         # Matrix multiply vectorization magic.
-        patterns: np.ndarray = _wordle_compare(
-            words[:, np.newaxis], words, pbar)
-    logger.info('Writing patterns to %s', _PATTERN_CACHE_FILE)
-    np.savez_compressed(_PATTERN_CACHE_FILE, patterns)
+        patterns: np.ndarray = cmp_pat(words[:, np.newaxis], words, pbar)
+    logger.info('Writing patterns to cache %s', _PATTERN_CACHE_FILE)
+    np.save(_PATTERN_CACHE_FILE, patterns)
+    logger.info('Writing patterns to archive %s', _PATTERN_ARCHIVE_FILE)
+    np.savez_compressed(_PATTERN_ARCHIVE_FILE, patterns)
 
 
 def load_patterns() -> np.ndarray:
     """Load already compiled patterns (n, n) from archive."""
-    logger.info('Loading pre-compiled patterns from %s', _PATTERN_CACHE_FILE)
-    return np.load(_PATTERN_CACHE_FILE)['arr_0']
+    if path.exists(_PATTERN_CACHE_FILE):
+        logger.info('Loading pre-compiled cache %s', _PATTERN_CACHE_FILE)
+        return np.load(_PATTERN_CACHE_FILE)
+    logger.info('No pattern cache %s found', _PATTERN_CACHE_FILE)
+
+    if not path.exists(_PATTERN_ARCHIVE_FILE):
+        logger.info('No pattern archive %s found', _PATTERN_ARCHIVE_FILE)
+        chunk_files = listdir(_CHUNK_DIR)
+        assert chunk_files, \
+            'Missing saved archive partitions. Run compile_patterns.'
+        chunk_files.sort()
+
+        logger.info('Joining %d binary chunks from %s',
+                    len(chunk_files), _CHUNK_DIR)
+        with open(_PATTERN_ARCHIVE_FILE, 'wb') as fdst:
+            for chunk in chunk_files:
+                chunk_path = path.join(_CHUNK_DIR, chunk)
+                with open(chunk_path, 'rb') as fsrc:
+                    copyfileobj(fsrc, fdst)
+
+    logger.info('Loading pre-compiled archive %s', _PATTERN_ARCHIVE_FILE)
+    patterns = np.load(_PATTERN_ARCHIVE_FILE)['arr_0']
+    logger.info('Writing patterns to cache %s', _PATTERN_CACHE_FILE)
+    np.save(_PATTERN_CACHE_FILE, patterns)
+    return patterns
