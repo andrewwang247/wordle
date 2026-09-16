56d4dd71b85570ec2ef6265f9ca2456d13eb832f
Andrew Wang <siweiw9@gmail.com>
2026-09-16 12:01:08 -0700

    pass input output files as args

--- static_cpp.sh
+++ static_cpp.sh
@@ -18,6 +18,7 @@ cppcheck --language=c++ --std=c++23 --quiet \
     --template=gcc \
     --suppress=checkersReport \
     --suppress=missingIncludeSystem \
+    --suppress=throwInEntryPoint \
     $sources
 
 if [[ ! -f "compile_commands.json" ]]; then
