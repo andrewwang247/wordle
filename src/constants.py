2a160f8c05a65685e72c76a6da8f39fb61102229
Andrew Wang <siweiw9@gmail.com>
2026-09-16 15:55:27 -0700

    significantly optimize wordle_compare by removing numpy use

--- src/constants.py
+++ src/constants.py
@@ -42,32 +42,38 @@ def convert_squares(squares: bytes) -> str:
     return "".join(values)
 
 
-def wordle_compare(
-    guess: bytes,
-    answer: bytes,
-    pbar: tqdm[Any] | None = None,
-) -> bytes:
+def wordle_compare(guess: bytes, answer: bytes, pbar: tqdm[Any] | None = None) -> bytes:
     """Given a guess and an answer, generate the squares pattern.
 
-    This runs in a very tight vectorized loop during compilation.
+    This runs in a very tight loop during cache generation.
     """
-    assert len(guess) == len(answer), "Guess and answer must have matching lengths"
-    guess_np: ByteArr = np.frombuffer(guess, dtype="S1")
-    answer_np: ByteArr = np.frombuffer(answer, dtype="S1")
-    squares: ByteArr = np.full_like(guess_np, b"b", dtype="S1")
+    squares = bytearray(b"b" * len(guess))
 
     # Mark green squares by position matching.
-    mismatch = guess_np != answer_np
-    squares[~mismatch] = b"g"
+    # Also validates len(guess) == len(answer)
+    for i, (g, a) in enumerate(zip(guess, answer, strict=True)):
+        if g == a:
+            squares[i] = ord(b"g")
 
-    # Iterate over unique non-green letters in guess.
-    for letter in np.unique(guess_np[mismatch]):
+    # Iterate over non-green letters in guess.
+    for i, letter in enumerate(guess):
+        if squares[i] == ord(b"g"):
+            continue
         # Count non-green appearances of letter in answer.
-        max_yellow = np.count_nonzero(answer_np[mismatch] == letter)
+        max_yellow = sum(
+            1
+            for an, sq in zip(answer, squares, strict=True)
+            if sq != ord(b"g") and an == letter
+        )
         # Color up to max_yellow squares where guess matches letter.
-        possible_idx = np.where(mismatch & (guess_np == letter))[0]
-        squares[possible_idx[:max_yellow]] = b"y"
+        num_yellow = 0
+        for j, gs in enumerate(guess):
+            if num_yellow == max_yellow:
+                break
+            if gs == letter and squares[j] != ord(b"g"):
+                squares[j] = ord(b"y")
+                num_yellow += 1
 
     if pbar:
         pbar.update(1)
-    return squares.tobytes()
+    return bytes(squares)
